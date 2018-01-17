cdea8f19fe11273c3f6de517958b66e664114d28
Tanzhu Azis <tanjuozen90@gmail.com>
2018-01-17 23:22:08 +0100

    Adding color generation to the squares and making the code a bit more modular.

--- Section15/colorGame.js
+++ Section15/colorGame.js
@@ -1,16 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,18 +13,18 @@ for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
 
     // add click listeners to squares
-    squares[i].addEventListener("click", function() {
-    // grab color of clicked square 
-    var clickedColor = this.style.backgroundColor;
-    // compare color to pickedColor
-
-    if (clickedColor === pickedColor) {
-        messageDisplay.textContent = "Correct!";
-        changeColors(clickedColor);
-    } else {
-        this.style.backgroundColor = "#232323";
-        messageDisplay.textContent = "Try Again";
-    }
+    squares[i].addEventListener("click", function () {
+        // grab color of clicked square 
+        var clickedColor = this.style.backgroundColor;
+        // compare color to pickedColor
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
     });
 }
 
@@ -42,7 +36,32 @@ function changeColors(color) {
     // change each color to match given color
 }
 
+// pick an index from the colors array
 function pickColor() {
-    var random =  Math.floor(Math.random() * colors.length);
+    var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+// generate random rgb colors to be assigned to starting squares
+function generateRandomColors(num) {
+    // make an array
+    var arr = [];
+    // generate random colors to array
+    for (var i = 0; i < num; i++) {
+        // get random color and push it to array
+        arr.push(randomColor());
+    }
+    // return the array
+    return arr;
+}
+// generate a single rgb color to be pushed to the colors array
+function randomColor() {
+    // pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    // pick a "green" from 0 - 255
+    var g = Math.floor(Math.random() * 256);
+    // pick a "blue" from 0 - 255
+    var b = Math.floor(Math.random() * 256);
+    // rgb(r, g, b)    
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
