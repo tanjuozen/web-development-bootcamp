d5c3b06fa713df72dd8c53eda62f21a593947840
Tanzhu Azis <tanjuozen90@gmail.com>
2018-01-21 00:27:13 +0100

    Styling the Todo list app and finishing it

--- Section18/assets/js/todos.js
+++ Section18/assets/js/todos.js
@@ -24,7 +24,11 @@ $("input[type='text']").keypress(function (event) {
         var todoText = $(this).val();
         $(this).val("");
         // create a new li and add to url
-        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
     }
 
+});
+
+$(".fa-plus").click(function() {
+    $("input[type='text']").fadeToggle();
 });
