dec4aa2d5b8c646ca3441432cf7e030e777f1bc7
Tanzhu Azis <tanjuozen90@gmail.com>
2018-04-15 18:14:11 +0200

    Adding a patatap clone

--- Section18/assets/js/todos.js
+++ Section18/assets/js/todos.js
@@ -1,34 +1,36 @@
 // check off specific todos by clicking
-$("ul").on("click", "li", function () {
-    // make the <li>s grey and line-through with toggle
-    $(this).toggleClass("completed");
-
+$("ul").on("click", "li", function() {
+  // make the <li>s grey and line-through with toggle
+  $(this).toggleClass("completed");
 });
 
 // click on x to delete todo
 /* we use ul in the first selection and add span as a parameter inside callback function
 the reason to do that, when the page loads there might be spans or not. 
 it's kind of future listening of spans under <ul> */
-$("ul").on("click", "span", function (event) {
-    // stop the event bubbling to go until the root html element
-    $(this).parent().fadeOut(1000, function () {
-        // remove the parent
-        $(this).remove();
-    })
-    event.stopPropagation();
+$("ul").on("click", "span", function(event) {
+  $(this)
+    .parent()
+    .fadeOut(1000, function() {
+      // remove the parent
+      $(this).remove();
+    });
+  // stop the event bubbling to go until the root html element
+  event.stopPropagation();
 });
 
-$("input[type='text']").keypress(function (event) {
-    if (event.which === 13) {
-        // grabbing the new todo from the input
-        var todoText = $(this).val();
-        $(this).val("");
-        // create a new li and add to url
-        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
-    }
-
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    // grabbing the new todo from the input
+    var todoText = $(this).val();
+    $(this).val("");
+    // create a new li and add to url
+    $("ul").append(
+      "<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>"
+    );
+  }
 });
 
 $(".fa-plus").click(function() {
-    $("input[type='text']").fadeToggle();
+  $("input[type='text']").fadeToggle();
 });
