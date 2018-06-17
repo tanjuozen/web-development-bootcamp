186c963ce05ee745e4478d65cb3db6a552064e33
Tanzhu Azis <tanjuozen90@gmail.com>
2018-06-17 22:03:56 +0200

    YelpCamp authentication
    - Adding logged user information globally to the application
    - Little tweaks in the navbar according to the logged user

--- Section26/YelpCamp/app.js
+++ Section26/YelpCamp/app.js
@@ -32,6 +32,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get("/", (req, res) => {
   res.render("landing");
 });
@@ -43,8 +48,7 @@ app.get("/campgrounds", (req, res) => {
       console.log(err);
     } else {
       res.render("campgrounds/index", {
-        campgrounds,
-        campgrounds
+        campgrounds: campgrounds
       });
     }
   });
@@ -84,7 +88,6 @@ app.get("/campgrounds/:id", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      console.log(foundCampground);
       // Render show template with that campground
       res.render("campgrounds/show", {
         campground: foundCampground
