e3f0a536767778361f2fbaa96b3fee77c45a1881
Tanzhu Azis <tanjuozen90@gmail.com>
2018-06-17 14:03:34 +0200

    Authentication
    - Adding User model to work with authentication
    - Adding passsportLocalMongoose to User model to be able to use methods like passport.serializeUser(User.serializeUser());

--- Section32/Authentication/app.js
+++ Section32/Authentication/app.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var LocalStrategy = require('passport-local');
+var passportLocalMongoose = require('passport-local-mongoose');
+var User = require('./models/user');
 
 mongoose.connect("mongodb://127.0.0.1:27017/auth_demo_app");
 
 var app = express();
+
+app.use(require('express-session')({
+    secret: "This is a secret for the session",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Coming from UserSchema.plugin(passportLocalMongoose)
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", (req, res) => {
     res.render("home");
