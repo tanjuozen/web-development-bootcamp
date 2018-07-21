b3b7ac191047e90f4c75be60d677c8e918240485
Tanzhu Azis <tanjuozen90@gmail.com>
2018-07-22 00:12:31 +0200

    Campground-User association

--- Section26/YelpCamp/routes/campgrounds.js
+++ Section26/YelpCamp/routes/campgrounds.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({
+    mergeParams: true
+});
 var Campground = require('../models/campground');
 
 // INDEX - Show all campgrounds
@@ -16,29 +18,35 @@ router.get("/", (req, res) => {
 });
 
 // CREATE - add new campground to DB
-router.post("/", (req, res) => {
+router.post("/", isLoggedIn, (req, res) => {
     // get data from form and add to campgrounds array
     // redirect back to / route
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
     var newCampground = {
         name: name,
         image: image,
-        description: description
+        description: description,
+        author: author
     };
     Campground.create(newCampground, (err, newlyCreated) => {
         if (err) {
             console.log(err);
         } else {
             //redirect as a get request
+            console.log(newlyCreated);
             res.redirect("/campgrounds");
         }
     });
 });
 
 // NEW - Show form to create new campground
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("campgrounds/new");
 });
 
@@ -56,5 +64,12 @@ router.get("/:id", (req, res) => {
         }
     });
 });
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
